5b10d2a7f26aec2678b09296008d354b7e68783e
prateek khurmi <115011912+Pki03@users.noreply.github.com>
2025-12-02 14:24:50 +0530

    n

--- day5/CF363B.java
+++ day5/CF363B.java
@@ -1,43 +1,34 @@
 import java.util.*;
 
 public class CF363B {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int k = s.nextInt();
 
-    public static void main(String[] args) 
-    {
-        Scanner s= new  Scanner(System.in);
-        int n= s.nextInt();
-        int m= s.nextInt();
-
-        int[] arr= new int[n];
-        for(int i=0;i<m;i++)
-        {
-            arr[i]=s.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
         }
 
-        int x=0;
-        int sum=0;
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<m;i++)
-        {
-            sum+=arr[i];
-
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
         }
-        min=sum;
 
-        for(int i=m;i<n;i++)
-        {
-            sum+=arr[i];
-            sum-=arr[i-m];
-            if(sum<min)
-            {
-                min=sum;
-                x=i-m+1;
+        int min = sum;
+        int start = 0; // 0-based index
+
+        for (int i = k; i < n; i++) {
+            sum += arr[i];
+            sum -= arr[i - k];
+
+            if (sum < min) {
+                min = sum;
+                start = i - k + 1;
             }
-            
         }
 
-        System.out.println(x);
-
+        System.out.println(start + 1); // convert to 1-based
     }
-    
 }
